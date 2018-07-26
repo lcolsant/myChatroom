96aa1cc56580d51378190b434cc126f94af08284
Lee Colsant <lcolsant@Lees-MacBook-Pro.local>
2018-07-26 13:05:39 -0700

    updating files

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-const port = 8000;
+const port = process.env.PORT || 8000;
 
 const app = express();
 const chatHistory = [];
